oleksii-novikov-onix/fineract

FINERACT-2455: add deliquency pause to Working Capital

fineract-core/src/main/java/org/apache/fineract/commands/service/CommandWrapperBuilder.java
@@ -605,6 +605,15 @@ public class CommandWrapperBuilder {
         return this;
     }
 
+    public CommandWrapperBuilder createWcLoanDelinquencyAction(final Long wcLoanId) {
+        this.actionName = "CREATE";
+        this.entityName = "WC_DELINQUENCY_ACTION";
+        this.entityId = wcLoanId;
+        this.loanId = wcLoanId;
+        this.href = "/working-capital-loans/" + wcLoanId + "/delinquency-actions";
+        return this;
+    }
+
     public CommandWrapperBuilder createClientIdentifier(final Long clientId) {
         this.actionName = "CREATE";
         this.entityName = "CLIENTIDENTIFIER";

fineract-working-capital-loan/src/main/java/org/apache/fineract/portfolio/workingcapitalloan/api/InternalWorkingCapitalLoanApiResource.java
@@ -29,15 +29,27 @@ import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.fineract.infrastructure.core.boot.FineractProfiles;
+import org.apache.fineract.infrastructure.core.service.DateUtils;
+import org.apache.fineract.portfolio.loanaccount.domain.LoanStatus;
 import org.apache.fineract.portfolio.workingcapitalloan.data.ProjectedAmortizationScheduleGenerateRequest;
+import org.apache.fineract.portfolio.workingcapitalloan.domain.WorkingCapitalLoan;
+import org.apache.fineract.portfolio.workingcapitalloan.domain.WorkingCapitalLoanDisbursementDetails;
+import org.apache.fineract.portfolio.workingcapitalloan.exception.WorkingCapitalLoanNotFoundException;
+import org.apache.fineract.portfolio.workingcapitalloan.repository.WorkingCapitalLoanRepository;
+import org.apache.fineract.portfolio.workingcapitalloan.service.WcLoanDelinquencyRangeScheduleService;
 import org.apache.fineract.portfolio.workingcapitalloan.service.WorkingCapitalLoanAmortizationScheduleWriteService;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -48,6 +60,8 @@ import org.springframework.stereotype.Component;
 public class InternalWorkingCapitalLoanApiResource implements InitializingBean {
 
     private final WorkingCapitalLoanAmortizationScheduleWriteService writeService;
+    private final WorkingCapitalLoanRepository loanRepository;
+    private final WcLoanDelinquencyRangeScheduleService rangeScheduleService;
 
     @Override
     @SuppressFBWarnings("SLF4J_SIGN_ONLY_FORMAT")
@@ -76,5 +90,64 @@ public class InternalWorkingCapitalLoanApiResource implements InitializingBean {
     public void generateAmortizationSchedule(@PathParam("loanId") @Parameter(description = "loanId") final Long loanId,
             final ProjectedAmortizationScheduleGenerateRequest request) {
         writeService.generateAndSaveAmortizationSchedule(loanId, request);
+    }
+
+    @POST
+    @Transactional
+    @Path("{loanId}/activate")
+    @Consumes({ MediaType.APPLICATION_JSON })
+    @Produces({ MediaType.APPLICATION_JSON })
+    @Operation(summary = "Activate a Working Capital Loan (testing only)", description = """
+            Sets the WC loan status to ACTIVE and records a disbursement detail with the given date.
+            Also generates the initial delinquency range schedule period if a delinquency bucket is configured.
+
+            DO NOT USE THIS IN PRODUCTION! Disbursement must go through the proper disbursement flow.""")
+    @ApiResponses({ @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "404", description = "Working Capital Loan not found") })
+    public Response activateLoan(@PathParam("loanId") @Parameter(description = "loanId") final Long loanId,
+            @QueryParam("disbursementDate") @Parameter(description = "Disbursement date (yyyy-MM-dd)") final String disbursementDateStr) {
+        WorkingCapitalLoan loan = loanRepository.findById(loanId).orElseThrow(() -> new WorkingCapitalLoanNotFoundException(loanId));
+
+        LocalDate disbursementDate = disbursementDateStr != null ? LocalDate.parse(disbursementDateStr, DateTimeFormatter.ISO_LOCAL_DATE)
+                : DateUtils.getBusinessLocalDate();
+
+        WorkingCapitalLoanDisbursementDetails detail = new WorkingCapitalLoanDisbursementDetails();
+        detail.setWcLoan(loan);
+        detail.setActualDisbursementDate(disbursementDate);
+        detail.setActualAmount(loan.getApprovedPrincipal());
+        loan.getDisbursementDetails().add(detail);
+
+        loan.setLoanStatus(LoanStatus.ACTIVE);
+        loanRepository.saveAndFlush(loan);
+
+        rangeScheduleService.generateInitialPeriod(loan);
+
+        log.info("Activated WC loan {} with disbursement date {} (TEST ONLY)", loanId, disbursementDate);
+        return Response.ok().build();
+    }
+
+    @POST
+    @Transactional
+    @Path("{loanId}/generate-next-delinquency-period")
+    @Consumes({ MediaType.APPLICATION_JSON })
+    @Produces({ MediaType.APPLICATION_JSON })
+    @Operation(summary = "Generate next delinquency range schedule period (testing only)", description = """
+            Generates the next delinquency range schedule period if the business date \
+            has passed the current period's toDate.
+
+            DO NOT USE THIS IN PRODUCTION! Period generation must go through COB.""")
+    @ApiResponses({ @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "404", description = "Working Capital Loan not found") })
+    public Response generateNextDelinquencyPeriod(@PathParam("loanId") @Parameter(description = "loanId") final Long loanId,
+            @QueryParam("businessDate") @Parameter(description = "Business date (yyyy-MM-dd)") final String businessDateStr) {
+        WorkingCapitalLoan loan = loanRepository.findById(loanId).orElseThrow(() -> new WorkingCapitalLoanNotFoundException(loanId));
+
+        LocalDate businessDate = businessDateStr != null ? LocalDate.parse(businessDateStr, DateTimeFormatter.ISO_LOCAL_DATE)
+                : DateUtils.getBusinessLocalDate();
+
+        rangeScheduleService.generateNextPeriodIfNeeded(loan, businessDate);
+
+        log.info("Generated next delinquency period for WC loan {} with business date {} (TEST ONLY)", loanId, businessDate);
+        return Response.ok().build();
     }
 }

gccarlosayala-eng/dev-fintech

fineract-core/src/main/java/org/apache/fineract/commands/service/CommandWrapperBuilder.java
@@ -605,6 +605,15 @@ public class CommandWrapperBuilder {
         return this;
     }
 
+    public CommandWrapperBuilder createWorkingCapitalLoanDelinquencyAction(final Long workingCapitalLoanId) {
+        this.actionName = "CREATE";
+        this.entityName = "WC_DELINQUENCY_ACTION";
+        this.entityId = workingCapitalLoanId;
+        this.loanId = workingCapitalLoanId;
+        this.href = "/working-capital-loans/" + workingCapitalLoanId + "/delinquency-actions";
+        return this;
+    }
+
     public CommandWrapperBuilder createClientIdentifier(final Long clientId) {
         this.actionName = "CREATE";
         this.entityName = "CLIENTIDENTIFIER";

fineract-working-capital-loan/src/main/java/org/apache/fineract/portfolio/workingcapitalloan/api/InternalWorkingCapitalLoanApiResource.java
@@ -29,15 +29,27 @@ import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.fineract.infrastructure.core.boot.FineractProfiles;
+import org.apache.fineract.infrastructure.core.service.DateUtils;
+import org.apache.fineract.portfolio.loanaccount.domain.LoanStatus;
 import org.apache.fineract.portfolio.workingcapitalloan.data.ProjectedAmortizationScheduleGenerateRequest;
+import org.apache.fineract.portfolio.workingcapitalloan.domain.WorkingCapitalLoan;
+import org.apache.fineract.portfolio.workingcapitalloan.domain.WorkingCapitalLoanDisbursementDetails;
+import org.apache.fineract.portfolio.workingcapitalloan.exception.WorkingCapitalLoanNotFoundException;
+import org.apache.fineract.portfolio.workingcapitalloan.repository.WorkingCapitalLoanRepository;
 import org.apache.fineract.portfolio.workingcapitalloan.service.WorkingCapitalLoanAmortizationScheduleWriteService;
+import org.apache.fineract.portfolio.workingcapitalloan.service.WorkingCapitalLoanDelinquencyRangeScheduleService;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -48,6 +60,8 @@ import org.springframework.stereotype.Component;
 public class InternalWorkingCapitalLoanApiResource implements InitializingBean {
 
     private final WorkingCapitalLoanAmortizationScheduleWriteService writeService;
+    private final WorkingCapitalLoanRepository loanRepository;
+    private final WorkingCapitalLoanDelinquencyRangeScheduleService rangeScheduleService;
 
     @Override
     @SuppressFBWarnings("SLF4J_SIGN_ONLY_FORMAT")
@@ -76,5 +90,64 @@ public class InternalWorkingCapitalLoanApiResource implements InitializingBean {
     public void generateAmortizationSchedule(@PathParam("loanId") @Parameter(description = "loanId") final Long loanId,
             final ProjectedAmortizationScheduleGenerateRequest request) {
         writeService.generateAndSaveAmortizationSchedule(loanId, request);
+    }
+
+    @POST
+    @Transactional
+    @Path("{loanId}/activate")
+    @Consumes({ MediaType.APPLICATION_JSON })
+    @Produces({ MediaType.APPLICATION_JSON })
+    @Operation(summary = "Activate a Working Capital Loan (testing only)", description = """
+            Sets the WC loan status to ACTIVE and records a disbursement detail with the given date.
+            Also generates the initial delinquency range schedule period if a delinquency bucket is configured.
+
+            DO NOT USE THIS IN PRODUCTION! Disbursement must go through the proper disbursement flow.""")
+    @ApiResponses({ @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "404", description = "Working Capital Loan not found") })
+    public Response activateLoan(@PathParam("loanId") @Parameter(description = "loanId") final Long loanId,
+            @QueryParam("disbursementDate") @Parameter(description = "Disbursement date (yyyy-MM-dd)") final String disbursementDateStr) {
+        WorkingCapitalLoan loan = loanRepository.findById(loanId).orElseThrow(() -> new WorkingCapitalLoanNotFoundException(loanId));
+
+        LocalDate disbursementDate = disbursementDateStr != null ? LocalDate.parse(disbursementDateStr, DateTimeFormatter.ISO_LOCAL_DATE)
+                : DateUtils.getBusinessLocalDate();
+
+        WorkingCapitalLoanDisbursementDetails detail = new WorkingCapitalLoanDisbursementDetails();
+        detail.setWcLoan(loan);
+        detail.setActualDisbursementDate(disbursementDate);
+        detail.setActualAmount(loan.getApprovedPrincipal());
+        loan.getDisbursementDetails().add(detail);
+
+        loan.setLoanStatus(LoanStatus.ACTIVE);
+        loanRepository.saveAndFlush(loan);
+
+        rangeScheduleService.generateInitialPeriod(loan);
+
+        log.info("Activated WC loan {} with disbursement date {} (TEST ONLY)", loanId, disbursementDate);
+        return Response.ok().build();
+    }
+
+    @POST
+    @Transactional
+    @Path("{loanId}/generate-next-delinquency-period")
+    @Consumes({ MediaType.APPLICATION_JSON })
+    @Produces({ MediaType.APPLICATION_JSON })
+    @Operation(summary = "Generate next delinquency range schedule period (testing only)", description = """
+            Generates the next delinquency range schedule period if the business date \
+            has passed the current period's toDate.
+
+            DO NOT USE THIS IN PRODUCTION! Period generation must go through COB.""")
+    @ApiResponses({ @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "404", description = "Working Capital Loan not found") })
+    public Response generateNextDelinquencyPeriod(@PathParam("loanId") @Parameter(description = "loanId") final Long loanId,
+            @QueryParam("businessDate") @Parameter(description = "Business date (yyyy-MM-dd)") final String businessDateStr) {
+        WorkingCapitalLoan loan = loanRepository.findById(loanId).orElseThrow(() -> new WorkingCapitalLoanNotFoundException(loanId));
+
+        LocalDate businessDate = businessDateStr != null ? LocalDate.parse(businessDateStr, DateTimeFormatter.ISO_LOCAL_DATE)
+                : DateUtils.getBusinessLocalDate();
+
+        rangeScheduleService.generateNextPeriodIfNeeded(loan, businessDate);
+
+        log.info("Generated next delinquency period for WC loan {} with business date {} (TEST ONLY)", loanId, businessDate);
+        return Response.ok().build();
     }
 }