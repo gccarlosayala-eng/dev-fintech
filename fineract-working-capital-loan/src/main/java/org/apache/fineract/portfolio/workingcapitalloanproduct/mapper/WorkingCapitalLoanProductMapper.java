oleksii-novikov-onix/fineract

FINERACT-2455: Add accounting configuration support for Working Capital loan products

fineract-working-capital-loan/src/main/java/org/apache/fineract/portfolio/workingcapitalloanproduct/mapper/WorkingCapitalLoanProductMapper.java
@@ -33,6 +33,7 @@ import org.apache.fineract.portfolio.workingcapitalloan.domain.WorkingCapitalLoa
 import org.apache.fineract.portfolio.workingcapitalloanproduct.data.WorkingCapitalLoanProductConfigurableAttributesData;
 import org.apache.fineract.portfolio.workingcapitalloanproduct.data.WorkingCapitalLoanProductData;
 import org.apache.fineract.portfolio.workingcapitalloanproduct.data.WorkingCapitalPaymentAllocationData;
+import org.apache.fineract.portfolio.workingcapitalloanproduct.domain.WCAccountingRuleType;
 import org.apache.fineract.portfolio.workingcapitalloanproduct.domain.WorkingCapitalAmortizationType;
 import org.apache.fineract.portfolio.workingcapitalloanproduct.domain.WorkingCapitalLoanProduct;
 import org.apache.fineract.portfolio.workingcapitalloanproduct.domain.WorkingCapitalLoanProductConfigurableAttributes;
@@ -63,6 +64,10 @@ public interface WorkingCapitalLoanProductMapper {
     @Mapping(target = "repaymentEvery", source = "relatedDetail.repaymentEvery")
     @Mapping(target = "repaymentFrequencyType", source = "relatedDetail.repaymentFrequencyType", qualifiedByName = "periodFrequencyTypeToStringEnumOptionData")
     @Mapping(target = "allowAttributeOverrides", source = "configurableAttributes", qualifiedByName = "configurableAttributesToData")
+    @Mapping(target = "accountingRule", source = "accountingRule", qualifiedByName = "accountingRuleToStringEnumOptionData")
+    @Mapping(target = "accountingMappings", ignore = true)
+    @Mapping(target = "accountingRuleOptions", ignore = true)
+    @Mapping(target = "accountingMappingOptions", ignore = true)
     @Mapping(target = "fundOptions", ignore = true)
     @Mapping(target = "currencyOptions", ignore = true)
     @Mapping(target = "amortizationTypeOptions", ignore = true)
@@ -120,6 +125,14 @@ public interface WorkingCapitalLoanProductMapper {
             return new WorkingCapitalPaymentAllocationData(rule.getTransactionType() != null ? rule.getTransactionType() : null,
                     paymentAllocationOrder);
         }).toList();
+    }
+
+    @Named("accountingRuleToStringEnumOptionData")
+    default StringEnumOptionData accountingRuleToStringEnumOptionData(final WCAccountingRuleType accountingRule) {
+        if (accountingRule == null) {
+            return null;
+        }
+        return accountingRule.toData();
     }
 
     @Named("configurableAttributesToData")

fineract-working-capital-loan/src/main/java/org/apache/fineract/portfolio/workingcapitalloanproduct/service/WorkingCapitalLoanProductWritePlatformServiceImpl.java
@@ -26,9 +26,11 @@ import java.util.List;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.apache.fineract.infrastructure.core.api.JsonCommand;
+import org.apache.fineract.infrastructure.core.data.ApiParameterError;
 import org.apache.fineract.infrastructure.core.data.CommandProcessingResult;
 import org.apache.fineract.infrastructure.core.data.CommandProcessingResultBuilder;
 import org.apache.fineract.infrastructure.core.domain.ExternalId;
+import org.apache.fineract.infrastructure.core.exception.PlatformApiDataValidationException;
 import org.apache.fineract.infrastructure.core.service.ExternalIdFactory;
 import org.apache.fineract.organisation.monetary.domain.MonetaryCurrency;
 import org.apache.fineract.portfolio.delinquency.domain.DelinquencyBucket;
@@ -40,6 +42,7 @@ import org.apache.fineract.portfolio.fund.exception.FundNotFoundException;
 import org.apache.fineract.portfolio.workingcapitalloan.domain.WorkingCapitalLoanPeriodFrequencyType;
 import org.apache.fineract.portfolio.workingcapitalloan.repository.WorkingCapitalLoanRepository;
 import org.apache.fineract.portfolio.workingcapitalloanproduct.WorkingCapitalLoanProductConstants;
+import org.apache.fineract.portfolio.workingcapitalloanproduct.domain.WCAccountingRuleType;
 import org.apache.fineract.portfolio.workingcapitalloanproduct.domain.WorkingCapitalAdvancedPaymentAllocationsJsonParser;
 import org.apache.fineract.portfolio.workingcapitalloanproduct.domain.WorkingCapitalAmortizationType;
 import org.apache.fineract.portfolio.workingcapitalloanproduct.domain.WorkingCapitalLoanProduct;
@@ -70,6 +73,7 @@ public class WorkingCapitalLoanProductWritePlatformServiceImpl implements Workin
     private final FundRepository fundRepository;
     private final DelinquencyBucketRepository delinquencyBucketRepository;
     private final WorkingCapitalAdvancedPaymentAllocationsJsonParser advancedPaymentAllocationsJsonParser;
+    private final WCProductAccountingMappingService wcAccountingMappingService;
 
     @Transactional
     @Override
@@ -88,6 +92,9 @@ public class WorkingCapitalLoanProductWritePlatformServiceImpl implements Workin
         final WorkingCapitalLoanProduct product = createProductFromCommand(fund, delinquencyBucket, command, paymentAllocationRules);
 
         this.repository.saveAndFlush(product);
+
+        // Create GL account mappings if accounting is enabled
+        this.wcAccountingMappingService.createAccountMapping(product.getId(), command);
 
         return new CommandProcessingResultBuilder() //
                 .withCommandId(command.commandId()) //
@@ -131,6 +138,28 @@ public class WorkingCapitalLoanProductWritePlatformServiceImpl implements Workin
 
         final Map<String, Object> changes = updateProductFields(product, command);
 
+        // Handle accounting rule update
+        if (command.parameterExists(WorkingCapitalLoanProductConstants.accountingRuleParamName)) {
+            final String newAccountingRuleValue = command
+                    .stringValueOfParameterNamed(WorkingCapitalLoanProductConstants.accountingRuleParamName);
+            if (newAccountingRuleValue == null || newAccountingRuleValue.isBlank()) {
+                throw new PlatformApiDataValidationException(List.of(ApiParameterError.parameterError(
+                        "validation.msg.WORKINGCAPITALLOANPRODUCT.accountingRule.cannot.be.blank",
+                        "The parameter `accountingRule` is mandatory.", WorkingCapitalLoanProductConstants.accountingRuleParamName)));
+            }
+            final WCAccountingRuleType newAccountingRule = WCAccountingRuleType.valueOf(newAccountingRuleValue);
+            final boolean accountingRuleChanged = newAccountingRule != product.getAccountingRule();
+
+            if (accountingRuleChanged) {
+                product.setAccountingRule(newAccountingRule);
+                changes.put(WorkingCapitalLoanProductConstants.accountingRuleParamName, newAccountingRuleValue);
+            }
+
+            final Map<String, Object> accountingMappingChanges = this.wcAccountingMappingService.updateAccountMapping(productId, command,
+                    accountingRuleChanged, newAccountingRule);
+            changes.putAll(accountingMappingChanges);
+        }
+
         if (!changes.isEmpty()) {
             this.repository.saveAndFlush(product);
         }
@@ -152,6 +181,7 @@ public class WorkingCapitalLoanProductWritePlatformServiceImpl implements Workin
             throw new WorkingCapitalLoanProductCannotBeDeletedException(productId);
         }
 
+        this.wcAccountingMappingService.deleteAccountMapping(productId);
         this.repository.delete(product);
 
         return new CommandProcessingResultBuilder() //
@@ -322,11 +352,17 @@ public class WorkingCapitalLoanProductWritePlatformServiceImpl implements Workin
         final WorkingCapitalLoanProductMinMaxConstraints minMaxConstraints = new WorkingCapitalLoanProductMinMaxConstraints(minPrincipal,
                 maxPrincipal, minPeriodPaymentRate, maxPeriodPaymentRate);
 
+        // Accounting
+        final String accountingRuleValue = command.parameterExists(WorkingCapitalLoanProductConstants.accountingRuleParamName)
+                ? command.stringValueOfParameterNamed(WorkingCapitalLoanProductConstants.accountingRuleParamName)
+                : WCAccountingRuleType.NONE.name();
+        final WCAccountingRuleType accountingRule = WCAccountingRuleType.valueOf(accountingRuleValue);
+
         // Configurable attributes
         final WorkingCapitalLoanProductConfigurableAttributes configurableAttributes = createConfigurableAttributesFromCommand(command);
 
         return new WorkingCapitalLoanProduct(name, shortName, externalId, fund, delinquencyBucket, startDate, closeDate, description,
-                currency, relatedDetail, minMaxConstraints, paymentAllocationRules, configurableAttributes);
+                accountingRule, currency, relatedDetail, minMaxConstraints, paymentAllocationRules, configurableAttributes);
     }
 
     private WorkingCapitalLoanProductConfigurableAttributes createConfigurableAttributesFromCommand(final JsonCommand command) {

gccarlosayala-eng/dev-fintech

fineract-working-capital-loan/src/main/java/org/apache/fineract/portfolio/workingcapitalloanproduct/mapper/WorkingCapitalLoanProductMapper.java
@@ -34,6 +34,7 @@ import org.apache.fineract.portfolio.workingcapitalloanbreach.mapper.WorkingCapi
 import org.apache.fineract.portfolio.workingcapitalloanproduct.data.WorkingCapitalLoanProductConfigurableAttributesData;
 import org.apache.fineract.portfolio.workingcapitalloanproduct.data.WorkingCapitalLoanProductData;
 import org.apache.fineract.portfolio.workingcapitalloanproduct.data.WorkingCapitalPaymentAllocationData;
+import org.apache.fineract.portfolio.workingcapitalloanproduct.domain.WorkingCapitalAccountingRuleType;
 import org.apache.fineract.portfolio.workingcapitalloanproduct.domain.WorkingCapitalAmortizationType;
 import org.apache.fineract.portfolio.workingcapitalloanproduct.domain.WorkingCapitalLoanDelinquencyStartType;
 import org.apache.fineract.portfolio.workingcapitalloanproduct.domain.WorkingCapitalLoanProduct;
@@ -68,6 +69,10 @@ public interface WorkingCapitalLoanProductMapper {
     @Mapping(target = "allowAttributeOverrides", source = "configurableAttributes", qualifiedByName = "configurableAttributesToData")
     @Mapping(target = "delinquencyGraceDays", source = "relatedDetail.delinquencyGraceDays")
     @Mapping(target = "delinquencyStartType", source = "relatedDetail.delinquencyStartType", qualifiedByName = "delinquencyStartTypeToStringEnumOptionData")
+    @Mapping(target = "accountingRule", source = "accountingRule", qualifiedByName = "accountingRuleToStringEnumOptionData")
+    @Mapping(target = "accountingMappings", ignore = true)
+    @Mapping(target = "accountingRuleOptions", ignore = true)
+    @Mapping(target = "accountingMappingOptions", ignore = true)
     @Mapping(target = "fundOptions", ignore = true)
     @Mapping(target = "currencyOptions", ignore = true)
     @Mapping(target = "amortizationTypeOptions", ignore = true)
@@ -133,6 +138,14 @@ public interface WorkingCapitalLoanProductMapper {
             return new WorkingCapitalPaymentAllocationData(rule.getTransactionType() != null ? rule.getTransactionType() : null,
                     paymentAllocationOrder);
         }).toList();
+    }
+
+    @Named("accountingRuleToStringEnumOptionData")
+    default StringEnumOptionData accountingRuleToStringEnumOptionData(final WorkingCapitalAccountingRuleType accountingRule) {
+        if (accountingRule == null) {
+            return null;
+        }
+        return accountingRule.toData();
     }
 
     @Named("configurableAttributesToData")

fineract-working-capital-loan/src/main/java/org/apache/fineract/portfolio/workingcapitalloanproduct/service/WorkingCapitalLoanProductWritePlatformServiceImpl.java
@@ -26,10 +26,12 @@ import java.util.List;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.apache.fineract.infrastructure.core.api.JsonCommand;
+import org.apache.fineract.infrastructure.core.data.ApiParameterError;
 import org.apache.fineract.infrastructure.core.data.CommandProcessingResult;
 import org.apache.fineract.infrastructure.core.data.CommandProcessingResultBuilder;
 import org.apache.fineract.infrastructure.core.domain.ExternalId;
 import org.apache.fineract.infrastructure.core.exception.GeneralPlatformDomainRuleException;
+import org.apache.fineract.infrastructure.core.exception.PlatformApiDataValidationException;
 import org.apache.fineract.infrastructure.core.service.ExternalIdFactory;
 import org.apache.fineract.organisation.monetary.domain.MonetaryCurrency;
 import org.apache.fineract.portfolio.delinquency.domain.DelinquencyBucket;
@@ -43,6 +45,7 @@ import org.apache.fineract.portfolio.workingcapitalloan.repository.WorkingCapita
 import org.apache.fineract.portfolio.workingcapitalloanbreach.domain.WorkingCapitalBreach;
 import org.apache.fineract.portfolio.workingcapitalloanbreach.repository.WorkingCapitalBreachRepository;
 import org.apache.fineract.portfolio.workingcapitalloanproduct.WorkingCapitalLoanProductConstants;
+import org.apache.fineract.portfolio.workingcapitalloanproduct.domain.WorkingCapitalAccountingRuleType;
 import org.apache.fineract.portfolio.workingcapitalloanproduct.domain.WorkingCapitalAdvancedPaymentAllocationsJsonParser;
 import org.apache.fineract.portfolio.workingcapitalloanproduct.domain.WorkingCapitalAmortizationType;
 import org.apache.fineract.portfolio.workingcapitalloanproduct.domain.WorkingCapitalLoanDelinquencyStartType;
@@ -75,6 +78,7 @@ public class WorkingCapitalLoanProductWritePlatformServiceImpl implements Workin
     private final DelinquencyBucketRepository delinquencyBucketRepository;
     private final WorkingCapitalAdvancedPaymentAllocationsJsonParser advancedPaymentAllocationsJsonParser;
     private final WorkingCapitalBreachRepository breachRepository;
+    private final WorkingCapitalProductAccountingMappingService wcAccountingMappingService;
 
     @Transactional
     @Override
@@ -98,6 +102,9 @@ public class WorkingCapitalLoanProductWritePlatformServiceImpl implements Workin
                 paymentAllocationRules);
 
         this.repository.saveAndFlush(product);
+
+        // Create GL account mappings if accounting is enabled
+        this.wcAccountingMappingService.createAccountMapping(product.getId(), command);
 
         return new CommandProcessingResultBuilder() //
                 .withCommandId(command.commandId()) //
@@ -142,6 +149,28 @@ public class WorkingCapitalLoanProductWritePlatformServiceImpl implements Workin
 
         final Map<String, Object> changes = updateProductFields(product, command);
 
+        // Handle accounting rule update
+        if (command.parameterExists(WorkingCapitalLoanProductConstants.accountingRuleParamName)) {
+            final String newAccountingRuleValue = command
+                    .stringValueOfParameterNamed(WorkingCapitalLoanProductConstants.accountingRuleParamName);
+            if (newAccountingRuleValue == null || newAccountingRuleValue.isBlank()) {
+                throw new PlatformApiDataValidationException(List.of(ApiParameterError.parameterError(
+                        "validation.msg.WORKINGCAPITALLOANPRODUCT.accountingRule.cannot.be.blank",
+                        "The parameter `accountingRule` is mandatory.", WorkingCapitalLoanProductConstants.accountingRuleParamName)));
+            }
+            final WorkingCapitalAccountingRuleType newAccountingRule = WorkingCapitalAccountingRuleType.valueOf(newAccountingRuleValue);
+            final boolean accountingRuleChanged = newAccountingRule != product.getAccountingRule();
+
+            if (accountingRuleChanged) {
+                product.setAccountingRule(newAccountingRule);
+                changes.put(WorkingCapitalLoanProductConstants.accountingRuleParamName, newAccountingRuleValue);
+            }
+
+            final Map<String, Object> accountingMappingChanges = this.wcAccountingMappingService.updateAccountMapping(productId, command,
+                    accountingRuleChanged, newAccountingRule);
+            changes.putAll(accountingMappingChanges);
+        }
+
         if (!changes.isEmpty()) {
             this.repository.saveAndFlush(product);
         }
@@ -164,6 +193,7 @@ public class WorkingCapitalLoanProductWritePlatformServiceImpl implements Workin
             throw new WorkingCapitalLoanProductCannotBeDeletedException(productId);
         }
 
+        this.wcAccountingMappingService.deleteAccountMapping(productId);
         this.repository.delete(product);
 
         return new CommandProcessingResultBuilder() //
@@ -351,11 +381,17 @@ public class WorkingCapitalLoanProductWritePlatformServiceImpl implements Workin
         final WorkingCapitalLoanProductMinMaxConstraints minMaxConstraints = new WorkingCapitalLoanProductMinMaxConstraints(minPrincipal,
                 maxPrincipal, minPeriodPaymentRate, maxPeriodPaymentRate);
 
+        // Accounting
+        final String accountingRuleValue = command.parameterExists(WorkingCapitalLoanProductConstants.accountingRuleParamName)
+                ? command.stringValueOfParameterNamed(WorkingCapitalLoanProductConstants.accountingRuleParamName)
+                : WorkingCapitalAccountingRuleType.NONE.name();
+        final WorkingCapitalAccountingRuleType accountingRule = WorkingCapitalAccountingRuleType.valueOf(accountingRuleValue);
+
         // Configurable attributes
         final WorkingCapitalLoanProductConfigurableAttributes configurableAttributes = createConfigurableAttributesFromCommand(command);
 
-        return new WorkingCapitalLoanProduct(name, shortName, externalId, fund, delinquencyBucket, breach, startDate, closeDate,
-                description, currency, relatedDetail, minMaxConstraints, paymentAllocationRules, configurableAttributes);
+        return new WorkingCapitalLoanProduct(name, shortName, externalId, fund, delinquencyBucket, startDate, closeDate, description,
+                accountingRule, currency, relatedDetail, minMaxConstraints, paymentAllocationRules, configurableAttributes, breach);
     }
 
     private WorkingCapitalBreach findBreachByIdIfProvided(final Long breachId) {